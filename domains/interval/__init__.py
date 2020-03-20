9d0e5894e1bf1fb040cf179e79d5bd3af7801b50
Matthew Sotoudeh <matthewsot@outlook.com>
2020-03-19 22:26:22 -0700

    More complete docstrings

--- domains/interval/__init__.py
+++ domains/interval/__init__.py
@@ -1,8 +1,4 @@
 """Module containing definitions for the Interval domain.
-
-This domain has an abstract space consisting of bottom, strictly positive,
-strictly negative, or top states for each variable in a program. The logical
-space is the set of conjunctions of one-variable inequalities against 0.
 """
 from .abstract import Interval, IntervalAbstractState
 from .domain import IntervalDomain
